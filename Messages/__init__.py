20b3c1a00f70e32c49fa3cc4cfa39d7c2fcadbc6
Mr-sdn <amirhoseinsdn@gmail.com>
2024-08-07 20:26:57 +0330

    initialize for set up language

---
+++ Messages/__init__.py
@@ -0,0 +1,38 @@
+import json
+
+fa: dict
+en: dict
+
+messages_fa = {}
+messages_en = {}
+
+
+def add_language_en() -> dict:
+
+    # add messages_fa to json file
+    with open('Messages/messages_en.json', 'w', encoding='utf-8') as f:
+        json.dump(messages_fa, f, ensure_ascii=False, indent=4)
+    with open('Messages/messages_en.json', 'r', encoding='utf-8') as f:
+        messages = json.load(f)
+    return messages
+
+
+def add_language_fa() -> dict:
+
+    # add messages_en to json file
+    with open('Messages/messages_fa.json', 'w', encoding='utf-8') as f:
+        json.dump(messages_en, f, ensure_ascii=False, indent=4)
+    with open('Messages/messages_fa.json', 'r', encoding='utf-8') as f:
+        messages = json.load(f)
+    return messages
+
+
+def main():
+    global en, fa
+    fa = add_language_fa()
+    en = add_language_en()
+
+main()
+
+
+
