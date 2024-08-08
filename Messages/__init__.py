58f2cea8283ad26ad4ee56110854ac681f49898b
Mr-sdn <amirhoseinsdn@gmail.com>
2024-08-08 14:32:19 +0330

    fix bug language: Message pkg

--- Messages/__init__.py
+++ Messages/__init__.py
@@ -15,7 +15,7 @@ def add_language_en() -> dict:
 
     # add messages_fa to json file
     with open('Messages/messages_en.json', 'w', encoding='utf-8') as f:
-        json.dump(_messages_fa, f, ensure_ascii=False, indent=4)
+        json.dump(_messages_en, f, ensure_ascii=False, indent=4)
     with open('Messages/messages_en.json', 'r', encoding='utf-8') as f:
         messages = json.load(f)
     return messages
@@ -25,7 +25,7 @@ def add_language_fa() -> dict:
 
     # add messages_en to json file
     with open('Messages/messages_fa.json', 'w', encoding='utf-8') as f:
-        json.dump(_messages_en, f, ensure_ascii=False, indent=4)
+        json.dump(_messages_fa, f, ensure_ascii=False, indent=4)
     with open('Messages/messages_fa.json', 'r', encoding='utf-8') as f:
         messages = json.load(f)
     return messages
