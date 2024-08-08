879f40ee1c1c249027f4a3b4c41823517433561f
Mr-sdn <amirhoseinsdn@gmail.com>
2024-08-08 13:03:26 +0330

    initialize bot module

---
+++ bot.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from Messages import en 
+
+api_id = "29071441" 
+api_hash = "dc0938f910a323e10afc005c4ffe9a68"
+bot_token = "2071989963:AAH_9mOgQ_YQVC-Og5p64jFg2-NGH3PbWVU"
+
+app = Client("smart-bot-channel", api_id=api_id, api_hash=api_hash)
+
+
+
+
+app.run()
