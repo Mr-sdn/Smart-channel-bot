40a021e484d326d1979074f181aef257c1a5fb15
Mr-sdn <amirhoseinsdn@gmail.com>
2024-08-09 10:01:09 +0330

    add setting to project

---
+++ Database/setting_database.py
@@ -0,0 +1,6 @@
+# settings of mysql database
+
+host = "localhost" # name of database server
+database = "smart" # database name
+user = "root"
+# password = your password needed
---
+++ setting_bot.py
@@ -0,0 +1,8 @@
+api_hash = "dc0938f910a323e10afc005c4ffe9a68"
+api_id = 29071441
+bot_token = "2071989963:AAH_9mOgQ_YQVC-Og5p64jFg2-NGH3PbWVU"
+proxy = { 
+"scheme": "socks5",  
+"hostname": "127.0.0.1",     
+"port" : 8086
+        }
