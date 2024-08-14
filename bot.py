efccee43873c91ff3ad07669026398fa3c1b4a3a
Mr-sdn <amirhoseinsdn@gmail.com>
2024-08-14 16:20:15 +0330

    handle query: add_channel

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+from pyrogram.types import Message, InlineQuery, InlineQueryResultArticle, InputTextMessageContent, CallbackQuery
 from Messages import en # you can import any language in Message pkg
 from Keyboards import keyboard_en # you can import any keyboard in Keyboards pkg
 from Database.database import Initial_main_table, check_new_user, add_new_user
@@ -16,6 +16,8 @@ proxy = {
 
 app = Client("smart-bot-channel", api_id=api_id, api_hash=api_hash, bot_token = bot_token, proxy=proxy)
 
+user_state = {}
+
 
 @app.on_message(filters.command(["start", "home"]) & filters.private)
 async def handle_welcome(client: Client, message: Message) -> None:
@@ -28,7 +30,22 @@ async def handle_welcome(client: Client, message: Message) -> None:
 @app.on_message(filters.regex(r"(^Channel settings 📣$)|(^تنظیمات کانال 📣$)"))
 async def handle_channel_settings(client: Client, message: Message) -> None:
     await message.reply(en["channel_settings"], reply_markup = keyboard_en.chennel_settings_menu)
-     
+
+
+@app.on_callback_query(filters.regex(r"^add_"))
+async def handle_add_query(client: Client, query: CallbackQuery):
+    result = query.data.split("_")[1]
+    id = query.from_user.id
+    if result == "channel":
+        await query.message.reply(en["get_channel_id"], reply_markup = keyboard_en.Cancel_operation_menu)
+        user_state[id] = "Waiting for the send channel"
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     Initial_main_table()
