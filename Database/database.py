fdf7121f8b33658880fb9be2650f1023cdbd1dfc
Mr-sdn <amirhoseinsdn@gmail.com>
2024-08-20 15:29:07 +0330

    get channels from database

--- Database/database.py
+++ Database/database.py
@@ -73,7 +73,6 @@ async def initial_user_table(user_id: int) -> None:
     # Create the user table by name user_id
     connection = await connect()
     cursor = connection.cursor()
-    target_id = user_id
     query_create_table_user = f"""CREATE TABLE `smart`.`{user_id}` 
     (`channels` VARCHAR(255) NOT NULL , `files` MEDIUMTEXT NOT NULL , `time` FLOAT NULL DEFAULT NULL , `repeated sending` VARCHAR(255) NOT NULL DEFAULT 'off' , `random sending` VARCHAR(255) NOT NULL DEFAULT 'off' ) ENGINE = InnoDB;"""
     cursor.execute(query_create_table_user)
@@ -106,7 +105,6 @@ async def check_new_channel(user_id: int, channel_id: str) -> bool:
     # check bot new user
     connection = await connect()
     cursor = connection.cursor()
-    target_id = channel_id
     query_exist = f"SELECT EXISTS(SELECT 1 FROM `{user_id}` WHERE channels = {channel_id})"
     cursor.execute(query_exist)
     exists = cursor.fetchone()[0]
@@ -116,4 +114,18 @@ async def check_new_channel(user_id: int, channel_id: str) -> bool:
         return True
     else:
         return False
-    
+
+
+async def get_channels(user_id: int):
+    # get channels from database
+    connection = await connect()
+    cursor = connection.cursor()
+    get_channel_query = f"""SELECT `channels` FROM `{user_id}` WHERE 1"""
+    cursor.execute(get_channel_query)
+    result = cursor.fetchall() 
+    list_channels = []
+    for channel in result:
+        list_channels.append(channel[0])
+    cursor.close()
+    connection.close()
+    return list_channels
